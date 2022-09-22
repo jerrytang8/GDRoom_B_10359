0a527b49357de8e41fe61817af51ab8907489c98
ASUS <jerrytang46@gmail.com>
2022-09-22 08:15:26 +0700

    guided room

---
+++ app/src/main/java/com/example/gdroom_b_10359/room/Note.kt
@@ -0,0 +1,11 @@
+package com.example.gdroom_b_10359.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val title: String,
+    val note:String )
